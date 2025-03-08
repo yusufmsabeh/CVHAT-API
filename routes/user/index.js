acbedfb4e48ecc766c6c646123e558d0f0fafee3
Yusuf Msabeh <yussef.msabeh@gmail.com>
2025-03-08 14:19:08 +0200

    feat: define and assign the CV route

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import auth from "./auth.js";
+import cv from "./cv.js";
 const router = new Router();
 router.use("/auth", auth);
+router.use("/cv", cv);
 export default router;
