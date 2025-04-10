9f3bba79563f4782dc1c6fd0df7b27abf7d9d83c
Mohammed Abu Motlaq <85284985+MohammedMotlaq@users.noreply.github.com>
2025-04-10 16:42:49 +0200

    feat: disable cache on test endpoint

--- controllers/test.js
+++ controllers/test.js
@@ -5,6 +5,12 @@ import {
 
 export const getTest = (req, res) => {
   try {
+    res.set({
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+      "Surrogate-Control": "no-store",
+    });
     return successResponse(res, 200, "server is running");
   } catch (error) {
     return serverSideErrorResponse(res, error);
